ccb1ccd160a3afd93ff7f5aa2f1fc74bbe7d8159
zack325546 <91444369+zack325546@users.noreply.github.com>
2021-11-15 03:56:17 +0800

    Create Class01.java

---
+++ Class01.java
@@ -0,0 +1,29 @@
+class Caaa
+{
+   public int num1;
+   public int num2;
+}
+
+class Cbbb extends Caaa
+{
+   public void set_num(int a,int b)
+   {
+      num1=a;
+      num2=b;
+   }
+   public void show()
+   {
+      System.out.println("num1="+num1);
+      System.out.println("num2="+num2);
+   }
+}
+
+public class Java01
+{
+   public static void main(String args[])
+   {
+      Cbbb bb=new Cbbb();
+      bb.set_num(5,10);
+      bb.show();
+   }
+}
